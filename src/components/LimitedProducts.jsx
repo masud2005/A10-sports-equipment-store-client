7b253f6162b1eb30700e503aef0b0ee365df1db1
masud2005 <masud20012005@gmail.com>
2025-01-07 11:02:27 +0600

    Explore Our Premium Sports Equipment Collection Section Layout Updated

--- src/components/LimitedProducts.jsx
+++ src/components/LimitedProducts.jsx
@@ -15,12 +15,12 @@ const LimitedProducts = () => {
     return (
         <div className="container mx-auto px-2 md:px-4 ">
             <div className="text-center mb-12 ">
-                <h2 className="text-4xl font-extrabold text-indigo-500 mb-4">Explore Our <span className="text-teal-500">Premium Sports Equipment Collection</span></h2>
+                <h2 className="text-4xl font-extrabold text-indigo-500 mb-1">Explore Our <span className="text-teal-500">Premium Sports Equipment Collection</span></h2>
                 <p className="text-lg text-gray-600 px-5">
                     Find top-quality gear for football, tennis, cricket, and more. Shop now to enhance your game!
                 </p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
                 {equipments.map(equipment => (
                     <div
                         key={equipment._id}
@@ -42,19 +42,19 @@ const LimitedProducts = () => {
                         <div className="flex-1 p-3 xl:p-6 flex flex-col justify-between">
                             <div>
                                 <h2 className="text-2xl md:text-3xl font-bold bg-gradient-to-r from-indigo-500 to-teal-500 text-transparent bg-clip-text mb-2">{equipment.itemName}</h2>
-                                <p className=" text-gray-500 mb-4">
-                                    {equipment.description.split(' ').slice(0, 20).join(' ')}...
+                                <p className=" text-gray-500 mb-1">
+                                    {equipment.description.split(' ').slice(0, 10).join(' ')}...
                                 </p>
-                                <div className="flex justify-between items-center mb-4">
+                                <div className="flex justify-between items-center mb-1">
                                     <p className="text-lg font-semibold text-teal-700">${equipment.price}</p>
                                     <p className=" font-medium text-gray-500">⭐ {equipment.rating}</p>
                                 </div>
-                                <p className=" text-gray-600 mb-4">
+                                <p className=" text-gray-600 mb-1">
                                     <span className="font-semibold">Stock:</span> {equipment.stockStatus} available
                                 </p>
-                                <p className=" text-gray-600 mb-4">
+                                {/* <p className=" text-gray-600 mb-1">
                                     <span className="font-semibold">Customization:</span> {equipment.customization}
-                                </p>
+                                </p> */}
                             </div>
                         </div>
 
